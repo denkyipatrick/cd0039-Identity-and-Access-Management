4b683467636a3e078908e4be26be4e8bb9a964a8
Patrick Denkyi <official.denkyipatrick@gmail.com>
2022-11-13 18:48:02 +0000

    chore: Set Auth0 parameters in the frontend environment

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-7l31l6kzr0o27sbt.us', // the auth0 domain prefix
+    audience: 'privoas.coffeeshop', // the audience set for the auth0 app
+    clientId: 'Kmaq8b2crqDg0hVAnZ2VqukbBnA8ybVI', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
